00fbb97c35c93c6fd5d4a31cd8383ca6cfde7162
Akash Kumar Mall <84437841+akashmall80@users.noreply.github.com>
2021-10-22 14:07:20 +0530

    Add files via upload

---
+++ upperandlowerlimitsinint.cpp
@@ -0,0 +1,12 @@
+//Check the Upper and Lower limit in an int and char
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    cout<<INT_MAX<<endl;
+    cout<<INT_MIN<<endl;
+    cout<<CHAR_MAX<<endl;
+    cout<<CHAR_MIN<<endl;
+    return 0;
+}
